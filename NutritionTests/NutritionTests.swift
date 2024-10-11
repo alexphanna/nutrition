dcfc39b0de000d715c628c88a2d5a3976713a27a
alexphanna <ahanna0608@gmail.com>
2024-10-11 15:09:12 -0400

    Initial Commit

---
+++ NutritionTests/NutritionTests.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionTests.swift
+//  NutritionTests
+//
+//  Created by alex on 10/11/24.
+//
+
+import Testing
+@testable import Nutrition
+
+struct NutritionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
